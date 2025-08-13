a237a15dc11ca1ab036856d9c2ee6418f616aa1b
nopeNoshishi <noah.of.havegreat@icloud.com>
2025-08-13 14:58:49 +0900

    lint code

--- src/storage.rs
+++ src/storage.rs
@@ -47,6 +47,7 @@ impl TokenStorage {
         self.expires_in >= Local::now().naive_utc()
     }
 
+    #[must_use]
     pub fn is_refresh_token_valid(&self) -> bool {
         self.refresh_expires_in
             .is_some_and(|exp| exp >= Local::now().naive_utc())
